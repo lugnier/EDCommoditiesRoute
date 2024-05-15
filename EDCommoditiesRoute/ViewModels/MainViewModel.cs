c570573be4e30bdb9c1d4f797df26143319abdbe
Benoit Lugnier <blugnier@index-education.fr>
2024-05-15 13:56:28 +0200

    1.0.0.1 : création de l'arborescence mvvm et création de la première fenêtre

---
+++ EDCommoditiesRoute/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDCommoditiesRoute.ViewModels
+{
+    internal partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public String titre;
+
+        #region CONSTRUCTOR
+        public MainViewModel()
+        {
+            Titre = "Ed Commodities Route";
+        }
+        #endregion
+    }
+}
